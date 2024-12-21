f72a01f4cb308ff3000ba26abcf25e6eac782a70
RiddhiSGithub <riddhimakwana77@gmail.com>
2024-12-21 14:18:17 -0500

    this is Wrapper class

---
+++ WrapperClass.java
@@ -0,0 +1,11 @@
+public class WrapperClass {
+
+    public static void main (String[] arg) {
+        //make integer as an object
+        Integer in = 10;
+
+        System.out.println(in.equals(10));
+
+        Double d = 10.00;
+    }
+}
